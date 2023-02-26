c8996d1b61c6bd43eb552d6fee21f0b81f02532d
ryan7984 <ryan7984@gmail.com>
2023-02-26 14:44:16 +0000

    new code

--- run.py
+++ run.py
@@ -1,117 +1,48 @@
 import random
 
-"""
-This function creates the board for the game.
-"""
-
-
 def create_board(size):
     board = []
     for i in range(size):
-        row = ["0"]* size
+        row = ["O"] * size
         board.append(row)
-    return board    
-
-
-"""
-This function prints the game board.
-"""
-
+    return board
 
 def print_board(board):
     for row in board:
-        print("".join(row))
+        print(" ".join(row))
 
+def random_row(board):
+    return random.randint(0, len(board) - 1)
 
-"""
-This function will place the ships randomly on the board
-"""
+def random_col(board):
+    return random.randint(0, len(board[0]) - 1)
 
-
-def place_ships(board, num_ships):
-    for i in range(num_ships):
-        ship_row = random.randint(0, len(board) - 1)
-        ship_col = random.randint(0, len(board[0]) - 1)
-        board[ship_row][ship_col] = "S"
-
-
-"""
-This function will check if the users guess is on the board.
-"""
-
-
-def valid_guess(guess, board):
-    row, col = guess
-    if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
-        return False
-    elif board[row][col] == "S":
-        return "hit"
-    elif board[row][col] == "x":
-        return "already guessed"
-    else:
-        return "miss"
-
-
-"""
-This function will check if the players guess is hit or miss.
-"""
-
-
-def check_guess(guess, board):
-    row, col = guess
-    if board[row][col] == "S":
-        return "hit"
-    elif board[row][col] == "X":
-        return "already guessed"
-    else:
-        return "miss"
-
-
-"""
-This function will update the board with the users guess
-"""
-
-
-def update_board(guess, board):
-    row, col = guess
-    if board[row][col] == "S":
-        board[row][col] = "X"
-        return True 
-    else:
-        board[row][col] = "."
-        return False
-
-
-"""
-This is the main function to run the game where the player enters the game
-board size, the number of ships and there guess.
-"""
-
-
-def play_battleships():
-    size = int(input("Enter the size of the game board:\n"))
-    num_ships = int(input("Enter the number of ships:\n"))
+def play_battleships(size):
     board = create_board(size)
-    place_ships(board, num_ships)
+    print("Let's play Battleships!")
     print_board(board)
-    num_guesses = 0
-    while True:
-        guess_str = input("Enter your guess (row, col): \n")
-        guess = tuple(map(int, guess_str.split(",")))
-        if not valid_guess(guess, size):
-            print("Invalid guess. Try again.")
-            continue
-        result = check_guess(guess, board)
-        if result == "Already guessed":
-            print("You already guessed that spot. Try again")
-            continue
-        num_guesses += 1
-        if update_board(guess, board):
-            print("Hit!")
-            if all(all(c != "S" for c in row) for row in board):
-                print("Congratulations! You sank all the battleships in",
-                      num_guesses, "guesses.")
-                break
+    ship_row = random_row(board)
+    ship_col = random_col(board)
 
-        else:   
-            print("Miss.")
+    for turn in range(4):
+        print("Turn", turn + 1)
+        guess_row = int(input("Guess Row: "))
+        guess_col = int(input("Guess Col: "))
+
+        if guess_row == ship_row and guess_col == ship_col:
+            print("Congratulations! You sank my battleship!")
+            break
+        else:
+            if guess_row not in range(size) or \
+               guess_col not in range(size):
+                print("Oops, that's not even in the ocean.")
+            elif board[guess_row][guess_col] == "X":
+                print("You guessed that one already.")
+            else:
+                print("You missed my battleship!")
+                board[guess_row][guess_col] = "X"
+            print_board(board)
+            if turn == 3:
+                print("Game Over")
+
+play_battleships(5)
